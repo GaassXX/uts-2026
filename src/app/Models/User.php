c75b1a9ad98ee42db4237eb6345cae2c02d5e52a
root <root@DESKTOP-7Q19K5N.localdomain>
2026-07-11 10:07:29 +0700

    Update

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -64,6 +64,6 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return true;
+        return $this->hasRole('super_admin');
     }
 }
