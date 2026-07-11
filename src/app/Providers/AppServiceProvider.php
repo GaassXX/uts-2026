122c12fa329e81d81a507ea6eae93f0c69510f5e
Ubuntu <dompetkuu@DompetKuu.hwa1grmdluveppzfa0xwjxmepa.jktx.internal.cloudapp.net>
2026-07-11 11:52:59 +0000

    Fix reverse proxy and Livewire production configuration

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
 {
     if (app()->environment('production')) {
-        URL::forceScheme('https');
+        //URL::forceScheme('https');
     }
 
     Gate::policy(Activity::class, ActivityPolicy::class);
